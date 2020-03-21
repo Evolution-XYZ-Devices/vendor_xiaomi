24505fe5427970a51ba3c84e8cd7484321a026e2
abhinavgupta371 <abhinavgupta371@gmail.com>
2020-03-21 06:42:11 +0000

    msm8953-common/tissot: Regen Vendor

--- tissot/Android.mk
+++ tissot/Android.mk
@@ -1,5 +1,5 @@
-# Copyright (C) 2017-2019 The LineageOS Project
-# Copyright (C) 2017-2019 The PixelExperience Project
+# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2020 The PixelExperience Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,27 +18,5 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),tissot)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libts_detected_face_hal
-LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES := proprietary/vendor/lib/libts_detected_face_hal.so
-LOCAL_MULTILIB := 32
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libts_face_beautify_hal
-LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES := proprietary/vendor/lib/libts_face_beautify_hal.so
-LOCAL_MULTILIB := 32
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
 
 endif
